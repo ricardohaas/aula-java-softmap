075f2d43cd1d73fa4fda510dc9882df0bea64911
Ricardo Haas <ricardo@voltsdigital.com.br>
2016-06-13 20:21:48 -0300

    Exercicio 6 - Tabuada

--- edu/softmap/Exercicio6.java
+++ edu/softmap/Exercicio6.java
@@ -1,8 +1,37 @@
 package edu.softmap;
 
+import javax.swing.JOptionPane;
+
 public class Exercicio6 {
 	public static void main( String[] Args){
+		int m = getNumberFromUser();
+		int n = getNumberFromUser();
+		Tabuada tabuada = new Tabuada();
+		tabuada.printMultiples( m, n );
 		
+	}
+	
+	public static int getNumberFromUser(){
+		int numberFromUser = 0;
+		while( true ){
+			
+			String dadoUsuario = JOptionPane.showInputDialog("Digite um número");
+			
+			try{
+				numberFromUser = Integer.parseInt( dadoUsuario );
+			}
+			catch( Exception ex){
+				System.out.println(ex);
+			}
+			
+			if( numberFromUser > 0 ){
+				System.out.println("Informado o número "+ numberFromUser + ".");
+				break;
+			}
+			
+			System.out.println( "Número inválido, por favor tente novamente");
+		} 
 		
+		return numberFromUser;
 	}
 }
--- edu/softmap/Tabuada.java
+++ edu/softmap/Tabuada.java
@@ -1,8 +1,18 @@
 package edu.softmap;
 
+import javax.swing.JOptionPane;
+
 public class Tabuada {
-	
-	public Tabuada(){
-		
+	/**
+	 * Print numbers multiples of m and lower n
+	 * @param m 
+	 * @param n
+	 */
+	public void printMultiples( int m, int n){
+		for( int i = 1; i < n; i++  ){
+			if( i % m == 0 ){
+				System.out.println( i+ " é um número múltiplo de " + m + " e menor que " + n);
+			}
+		}
 	}
 }
